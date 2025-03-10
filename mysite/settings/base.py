a4ce9958fc53c587d593f3ff12c0cad629f06054
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-03-10 10:28:55 -0400

    update:wagtail page

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-
+ALLOWED_HOSTS = ["*"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
